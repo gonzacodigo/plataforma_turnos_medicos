29f3c345db9091720bdef292752a45fd931fb947
Daniel Fabián <103130176+Dofdanicho@users.noreply.github.com>
2025-01-16 11:38:40 -0300

    Update main.py
    Realice los crud de doctors, schedules, specialities

--- flask_project/app/main.py
+++ flask_project/app/main.py
@@ -147,5 +147,140 @@ def get_administrators():
     users = cursor.fetchall()
     return jsonify(users)
 
+# Rutas para el CRUD de specialities
+# Realizado por Daniel
+
+@app.route('/specialities', methods=['GET'])
+def get_specialities():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM specialities')
+    specialities = cursor.fetchall()
+    return jsonify(specialities)
+
+
+@app.route('/specialities/<int:esp_id>', methods=['GET'])
+def get_speciality(esp_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM specialities WHERE esp_id = %s', (esp_id,))
+    speciality = cursor.fetchone()
+    return jsonify(speciality)
+
+@app.route('/specialities/<int:esp_id>', methods=['DELETE'])
+def delete_speciality(esp_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('DELETE FROM specialities WHERE esp_id = %s', (esp_id,))
+    mysql.connection.commit()
+    return jsonify({'message': 'Speciality deleted successfully'})
+
+@app.route('/specialities', methods=['POST'])
+def create_speciality():
+    data = request.get_json()
+    cursor = mysql.connection.cursor()
+    cursor.execute('INSERT INTO specialities (esp_name) VALUES (%s)', (data['esp_name'],))
+    mysql.connection.commit()
+    return jsonify({'message': 'Speciality created successfully'}),201
+
+@app.route('/specialities/<int:esp_id>', methods=['PUT'])
+def update_speciality(esp_id):
+    data = request.get_json()
+    esp_name = data['esp_name']
+    cursor = mysql.connection.cursor()
+    cursor.execute('UPDATE specialities SET esp_name = %s WHERE esp_id = %s', (esp_name, esp_id))
+    mysql.connection.commit()
+    return jsonify({'message': 'Speciality updated successfully'})
+
+
+
+# Rutas para el CRUD de schedules
+# Realizado por Daniel
+
+@app.route('/schedules', methods=['GET'])
+def get_schedules():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM schedules')
+    column_names = [desc[0] for desc in cursor.description]  
+    schedules = []
+    
+    for row in cursor.fetchall():
+        row_dict = dict(zip(column_names, row))
+
+        # Convierte timedelta a string (HH:MM:SS)
+        for key, value in row_dict.items():
+            if isinstance(value, datetime.timedelta):
+                row_dict[key] = str(value)  # Convertir timedelta a cadena
+
+        schedules.append(row_dict)
+
+    return jsonify(schedules)
+
+
+
+@app.route('/schedules', methods=['POST'])  
+def create_schedule():
+    data = request.get_json()
+    cursor = mysql.connection.cursor()
+    cursor.execute('INSERT INTO schedules (hor_dia, hor_franja, hor_duracion) VALUES (%s, %s, %s)', (data['hor_dia'], data['hor_franja'], data['hor_duracion']))
+    mysql.connection.commit()
+    return jsonify({'message': 'Schedule created successfully'}),201
+
+@app.route('/schedules/<int:hor_id>', methods=['PUT'])
+def update_schedule(hor_id):
+    data = request.get_json()
+    hor_dia = data['hor_dia']
+    hor_franja = data['hor_franja']
+    hor_duracion = data['hor_duracion']
+    cursor = mysql.connection.cursor()
+    cursor.execute('UPDATE schedules SET hor_dia = %s, hor_franja = %s, hor_duracion = %s WHERE hor_id = %s', (hor_dia, hor_franja, hor_duracion, hor_id))
+    mysql.connection.commit()
+    return jsonify({'message': 'Schedule updated successfully'})
+
+@app.route('/schedules/<int:hor_id>', methods=['DELETE'])
+def delete_schedule(hor_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('DELETE FROM schedules WHERE hor_id = %s', (hor_id,))
+    mysql.connection.commit()
+    return jsonify({'message': 'Schedule deleted successfully'})
+
+# Rutas para el CRUD de doctors
+# Realizado por Daniel
+
+@app.route('/doctors', methods=['GET'])
+def get_doctors():
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM doctors')
+    doctors = cursor.fetchall()
+    return jsonify(doctors)
+
+@app.route('/doctors', methods=['POST'])
+def create_doctor():
+    data = request.get_json()
+    doc_matricula = data['doc_matricula']
+    usr_id = data['usr_id']
+    esp_id = data['esp_id']
+    hor_id = data['hor_id']
+    cursor = mysql.connection.cursor()
+    cursor.execute('INSERT INTO doctors (doc_matricula, usr_id, esp_id, hor_id) VALUES (%s, %s,%s,%s)', (doc_matricula, usr_id, esp_id, hor_id))
+    mysql.connection.commit()
+    return jsonify({'message': 'Doctor created successfully'}),201
+
+@app.route('/doctors/<int:doc_id>', methods=['PUT'])
+def update_doctor(doc_id):
+    data = request.get_json()
+    doc_matricula = data['doc_matricula']
+    usr_id = data['usr_id']
+    esp_id = data['esp_id']
+    hor_id = data['hor_id']
+    cursor = mysql.connection.cursor()
+    cursor.execute('UPDATE doctors SET doc_matricula = %s, usr_id = %s, esp_id = %s, hor_id = %s WHERE doc_id = %s', (doc_matricula, usr_id, esp_id, hor_id, doc_id))
+    mysql.connection.commit()
+    return jsonify({'message': 'Doctor updated successfully'})
+
+@app.route('/doctors/<int:doc_id>', methods=['DELETE'])
+def delete_doctor(doc_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute('DELETE FROM doctors WHERE doc_id = %s', (doc_id,))
+    mysql.connection.commit()
+    return jsonify({'message': 'Doctor deleted successfully'})
+
 if __name__ == '__main__':
     app.run(debug=True)
